280b431ebd459ffbfe5c1f612ec6e3efbd89460e
guzhongren <guzhongren@live.cn>
2025-10-21 17:13:34 +0800

    feat: uv

--- bin/install.sh
+++ bin/install.sh
@@ -183,112 +183,9 @@ install_infra() {
     install_zsh;
     echo "🎉 Successfully installed infra..."
 }
- install_rime_ice() {
-    app_name="Rime_ICE"
-    echo "🍗 Start to install ${app_name}..."
-    git clone https://github.com/iDvel/rime-ice.git ~/Library/RimeRime --depth 1
-    echo "🎉 Successfully installed ${app_name}..."
- }
 
 ########################################base#######################################
-########################################asdf#######################################
 
-install_asdf_dependencies() {
-    echo "🍗 Start to install asdf dependencies..."
-
-
-
-    echo "🎉 All asdf dependencies are installed"
-}
-
-#######################################asdf#######################################
-#######################################zsh#######################################
-config_zsh() {
-cat << 'EOF' > "$HOME/.zshrc"
-    if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-        source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-    fi
-
-    export ZSH="$HOME/.oh-my-zsh"
-
-    ZSH_THEME="robbyrussell"
-
-    HIST_STAMPS="mm/dd/yyyy"
-
-    ZSH_CUSTOM=$ZSH/custom
-
-    plugins=(
-    git
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    zsh-completions
-    )
-
-    source $ZSH/oh-my-zsh.sh
-
-
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-    export CARGO_INCREMENTAL=0
-    export PATH="$HOME/.deno/bin:$PATH"
-    fpath=(~/.zsh $fpath)
-    autoload -Uz compinit
-    compinit -u
-
-    alias ls='lsd'
-    export GPG_TTY=$(tty)
-
-    export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.aliyun.com/homberew/brew.git"
-    export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/homebrew-core.git"
-    export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.aliyun.com/homebrew/homebrew-bottles"
-    export HOMEBREW_NO_INSTALL_CLEANUP=false
-    export HOMEBREW_NO_AUTO_UPDATE=true
-
-    export ZSH_DISABLE_COMPFIX=true
-
-    alias daily="cd '~/Library/Mobile Documents/iCloud~com~logseq~logseq/Documents'"
-
-    alias glol="git log --oneline --decorate --graph --date=local --pretty=format:'%C(yellow)%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset %C(white)%cd%Creset'"
-
-
-
-    # direnv
-    eval "$(direnv hook zsh)"
-
-    # enable to download node-pre-gyp package
-    export NODE_TLS_REJECT_UNAUTHORIZED=0
-
-    # alias docker="podman"
-    # export PODMAN_LOG_LEVEL=info
-    # export DOCKER_HOST=unix:///Users/zhongren.gu/.local/share/containers/podman/machine/podman.sock
-
-
-    # alias docker-compose="podman-compose"
-
-    # pnpm
-    export PNPM_HOME="~/Library/pnpm"
-    case ":$PATH:" in
-    *":$PNPM_HOME:"*) ;;
-    *) export PATH="$PNPM_HOME:$PATH" ;;
-    esac
-    # pnpm end
-
-    # SSH config
-    # Start the SSH agent if it's not already running
-    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-        eval "$(ssh-agent -s)"
-    fi
-
-    # Add the SSH key to the agent
-    # ssh-add ~/.ssh/hawcroft_bitbucket_id_ed25519
-    # End SSH config
-EOF
-}
-
-
-#######################################zsh#######################################
 #######################################show todo#######################################
 show_todo() {
   echo "I think you have installed some necessary softwares on your laptop now. But there are some things need to be done on your side.
@@ -324,6 +221,7 @@ cast_list=(
 
 tool_list=(
     "mise"
+    "uv"
     "colima"
     "wget"
     "curl"
@@ -343,6 +241,7 @@ languagetool_list=(
     "hugo"
     "pre-commit"
     "python"
+    "pnpm"
  )
 
 
@@ -351,12 +250,6 @@ install_infra;
 install_cask_list "${cast_list[@]}";
 
 install_tool_list "${tool_list[@]}";
-
-install_asdf_dependencies;
-
-config_zsh;
-
-install_rime_ice;
 
 install_languagetools "${languagetool_list[@]}";
 
